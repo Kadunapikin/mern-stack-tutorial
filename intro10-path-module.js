54e029e8188fbc58320f9d7e7d3b3423624aeb27
kadunapikin <ibakoshi27@gmail.com>
2023-09-20 16:29:15 +0100

    update path module

--- intro10-path-module.js
+++ intro10-path-module.js
@@ -10,3 +10,7 @@ console.log(filePath)
 //To get the path to the final file in the folder
 const base = path.basename(filePath);
 console.log(base);
+
+//To get the absolute path
+const absolute = path.resolve(__dirname, 'content', 'sub-folder', 'test.txt')
+console.log(absolute)
