72da9fe46480e2171c8d5f27f4b6c73a017b7920
kadunapikin <ibakoshi27@gmail.com>
2023-09-22 10:57:32 +0100

    update server synchronous event loop

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Synchonous setting up of codes and why its not good
 const http = require('http');
 
 const server = http.createServer((req, res) => {
@@ -5,6 +6,13 @@ const server = http.createServer((req, res) => {
         res.end('This is the landing page')
     }
     if (req.url === '/about') {
+        //After seeing that everything is working well, lets add some blocking code to show why it can waste all users time and not just the one making the request
+        //This make both the user on home page and the one on about page stay longer to get a response
+        for (let i = 0; i < 100; i++) {
+            for (let j = 0; j < 100; j++) {
+                console.log(`${i} and ${j}`);
+            }
+        }
         res.end('This is the about page')
     }
     res.end('This is an errpr page')
--- intro16-server-eventloop.js
+++ intro16-server-eventloop.js
@@ -1,8 +1,36 @@
+//First Example
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     console.log('Request Event');
+//     res.end('Hello World');
+// })
+
+// server.listen(5000, () => {
+//     console.log('The server is listening on port : 5000......');
+// })
+
+//second example
+
+//Synchonous setting up of codes and why its not good
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Request Event');
-    res.end('Hello World');
+    if (req.url === '/') {
+        res.end('This is the landing page')
+    }
+    if (req.url === '/about') {
+        //After seeing that everything is working well, lets add some blocking code to show why it can waste all users time and not just the one making the request
+        //This make both the user on home page and the one on about page stay longer to get a response
+        for (let i = 0; i < 100; i++) {
+            for (let j = 0; j < 100; j++) {
+                console.log(`${i} and ${j}`);
+            }
+        }
+        res.end('This is the about page')
+    }
+    res.end('This is an errpr page')
 })
 
 server.listen(5000, () => {
