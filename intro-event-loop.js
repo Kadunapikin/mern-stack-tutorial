78bedcaa21e31986f75614a1782518b688d74007
kadunapikin <ibakoshi27@gmail.com>
2023-09-22 07:51:30 +0100

    update nodeJs event loop

--- intro-event-loop.js
+++ intro-event-loop.js
@@ -8,3 +8,27 @@ setTimeout(() => {
 
 console.log('Where are you currently');
 console.log('Take care!');
+
+//second exaample
+
+const {readFile} = require('fs');
+
+console.log('starting my task')
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return err
+    }
+    console.log(result);
+    console.log('Done with first task!');
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if (err) {
+            console.log(err)
+            return err
+        }
+        console.log(result);
+        console.log('Done with second task!');
+    })
+})
+console.log('Completed task!');
+console.log('Starting the next one')
