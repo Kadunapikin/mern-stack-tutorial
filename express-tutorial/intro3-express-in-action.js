d880ed920aee602f1458d689aa0464fe7d82e738
kadunapikin <ibakoshi27@gmail.com>
2023-09-26 17:16:59 +0100

    express params

--- express-tutorial/intro3-express-in-action.js
+++ express-tutorial/intro3-express-in-action.js
@@ -5,9 +5,11 @@ const app = express();
 //setup static and midleware
 app.use(express.static('./public'));
 
-app.get('/', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './express-tutorial/index.html'));
-});
+//another method to send the html file will be to dump it on the public folder alongside other files and comment the app.get() method
+
+// app.get('/', (req,res) =>{
+//     res.sendFile(path.resolve(__dirname, './express-tutorial/index.html'));
+// });
 
 app.all('*', (req,res) =>{
     res.status(404).send('Resource Not Found')
