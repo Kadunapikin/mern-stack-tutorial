9d8301cab11e84e84d6f6240678479c917ddf601
kadunapikin <ibakoshi27@gmail.com>
2023-09-29 17:10:49 +0100

    serve the products id, name and image for clients

--- express-tutorial/intro4-express-params.js
+++ express-tutorial/intro4-express-params.js
@@ -1,5 +1,4 @@
-//To display single product
-
+//To map through products and return selected information
 const express = require('express');
 const app = express();
 const { products } = require('./data');
@@ -9,9 +8,34 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/products', (req, res) => {
-    res.json(products);
+    const newProduct = products.map((product) => {
+        const { id, name, image } = product
+        return { id, name, image };
+    })
+    res.json(newProduct);
+    console.log(newProduct);
 })
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000');
 })
+
+
+
+// //To display single product
+
+// const express = require('express');
+// const app = express();
+// const { products } = require('./data');
+
+// app.get('/', (req, res) => {
+//     res.send('<h1>Home page</h1><a href="/api/products">Products</a>');
+// })
+
+// app.get('/api/products', (req, res) => {
+//     res.json(products);
+// })
+
+// app.listen(5000, () => {
+//     console.log('Server is listening on port 5000');
+// })
